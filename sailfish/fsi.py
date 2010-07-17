569d46cf97bee121a37ed2f07511644df6c13b96
Michał Januszewski <spock@gentoo.org>
2010-07-17 19:25:57 +0200

    Fix particle orientation visualization.

--- sailfish/fsi.py
+++ sailfish/fsi.py
@@ -166,4 +166,4 @@ class SphericalParticle(FSIObject):
                 (scale_x + scale_y) / 2)
         pygame.draw.line(surf, (255, 0, 0), (pos_x, sh - pos_y),
                 (pos_x + self.radius * scale_x * math.cos(ang[0]),
-                 sh - pos_y + self.radius * scale_y * math.sin(ang[0])))
+                 sh - pos_y - self.radius * scale_y * math.sin(ang[0])))

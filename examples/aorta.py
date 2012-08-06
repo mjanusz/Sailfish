a40d6f2f74ad9ecd91aeea41e2885a53c130096e
Michał Januszewski <michalj@gmail.com>
2012-08-06 02:06:04 +0200

    Add support for pulsatile flow.

--- examples/aorta.py
+++ examples/aorta.py
@@ -6,9 +6,10 @@ import numpy as np
 from sailfish.geo import EqualSubdomainsGeometry3D
 from sailfish.subdomain import Subdomain3D
 from sailfish.node_type import NTFullBBWall, NTEquilibriumVelocity, DynamicValue, multifield
-from sailfish.controller import LBSimulationControlle
-from sailfish.lb_single import LBFluidSim
-from sailfish import sym
+from sailfish.controller import LBSimulationController
+from sailfish.lb_single import LBFluidSim, LBForcedSim
+from sailfish.sym import S
+from sympy import sin, cos
 
 class AortaSubdomain(Subdomain3D):
 
@@ -31,16 +32,17 @@ class AortaSubdomain(Subdomain3D):
 
         return partial_wall_map, inflow_map
 
-    def velocity_profile(self, hx, hy, hz, partial_wall_map, inflow_map):
+    def velocity_params(self, partial_wall_map):
         # This assumes that the whole inlet is contained in the current subdomain.
         inflow_plane = partial_wall_map[:,:,-1]
-
-        # Locate the geometric center of the inlet.
         z, y = np.where(np.logical_not(inflow_plane))
         zm = (min(z) + max(z)) / 2
         ym = (min(y) + max(y)) / 2
         diam = min(max(z) - min(z), max(y) - min(y))
+        return ym, zm, diam
 
+    def velocity_profile(self, hx, hy, hz, partial_wall_map, inflow_map):
+        ym, zm, diam = self.velocity_params(partial_wall_map)
         r = np.sqrt((hz - hz[zm, 0, 0])**2 + (hy - hy[0, ym, 0])**2)
         v = self.inflow_velocity / (diam / 2.0)**2 * ((diam / 2.0)**2 - r**2)
         return v
@@ -60,25 +62,35 @@ class AortaSubdomain(Subdomain3D):
             # Simplest (unphysical) case: constant inflow velocity everywhere at the inlet.
             # self.set_node(inflow_map, NTEquilibriumVelocity((self.inflow_velocity, 0.0, 0.0)))
 
-            v = self.velocity_profile(hx, hy, hz, partial_wall_map, inflow_map)
-            self.set_node(inflow_map, NTEquilibriumVelocity(multifield((v, 0.0, 0.0), inflow_map)))
+            # v = self.velocity_profile(hx, hy, hz, partial_wall_map, inflow_map)
+            # self.set_node(inflow_map, NTEquilibriumVelocity(multifield((v, 0.0, 0.0), inflow_map)))
 
+            ym, zm, diam = self.velocity_params(partial_wall_map)
+            zmm = hz[zm, 0, 0]
+            ymm = hy[0, ym, 0]
+
+            radius_sq = (diam / 2.0)**2
+            self.set_node(inflow_map, NTEquilibriumVelocity(DynamicValue(
+                cos(S.time) * self.inflow_velocity / radius_sq * (radius_sq - ((S.gz - zmm)**2 + (S.gy - ymm)**2)),
+                0.0, 0.0)))
             # sym.gy, sym.gz -> coordinates for DynamicValue
             # sym.time
 
 
-class AortaSimulation(LBFluidSim):
+class AortaSimulation(LBFluidSim, LBForcedSim):
     subdomain = AortaSubdomain
 
     @classmethod
     def update_defaults(cls, defaults):
         defaults.update({
             'grid': 'D3Q19',
+            'dt_per_lattice_time_unit': 0.001
             })
 
     @classmethod
     def add_options(cls, group, dim):
         LBFluidSim.add_options(group, dim)
+        LBForcedSim.add_options(group, dim)
 
         group.add_argument('--geometry', type=str,
                 help='file defining the geometry')
@@ -88,10 +100,9 @@ class AortaSimulation(LBFluidSim):
         if not config.geometry:
             return
 
-        wall_map = np.load(config.geometry)
-        # Get rid of the walls perpendicular to the direction of the flow.
-        wall_map = wall_map[:,:,1:-1]
         # Override lattice size based on the geometry file.
+        wall_map = np.load(config.geometry)
+        wall_map = wall_map[:,:,1:-1]  # get rid of the walls
         config.lat_nz, config.lat_ny, config.lat_nx = wall_map.shape
         config._wall_map = wall_map
 

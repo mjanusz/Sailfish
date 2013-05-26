7406b9b3bb636f901159ef8acc02a1e27b99361a
Michał Januszewski <michalj@gmail.com>
2013-05-26 20:22:48 +0200

    Temp changes.

--- utils/voxelizer/subdomain_test.cpp
+++ utils/voxelizer/subdomain_test.cpp
@@ -3,12 +3,43 @@
 
 using namespace std;
 
-TEST(DTreeNode, NodeLocation) {
+Octree MakeTestTree() {
 	Octree octree(0);
 	octree.expand('a');
 	for (int i = 0; i < 8; i++) {
-		(octree.root())[i]() = (char)('a' + i);
+		octree.root()[i]() = (char)('a' + i);
 	}
+
+	auto h = octree.root()[0];
+	h.expand('A');
+	for (int i = 0; i < 8; i++) {
+		h[i]() = (char)('A' + i);
+	}
+
+	h = octree.root()[5];
+	h.expand('0');
+	for (int i = 0; i < 8; i++) {
+		h[i]() = (char)('0' + i);
+	}
+	return octree;
+}
+
+TEST(DTreeNode, NodeLocation) {
+	Octree octree = MakeTestTree();
+
+	EXPECT_EQ(iPoint3D(2, 2, 2), NodeLocation(octree.root()));
+	EXPECT_EQ(iPoint3D(1, 1, 1), NodeLocation(octree.root()[0]));
+	EXPECT_EQ(iPoint3D(0, 0, 0), NodeLocation(octree.root()[0][0]));
+}
+
+TEST(DTreeNode, NodeExtent) {
+	Octree octree = MakeTestTree();
+
+	EXPECT_EQ(iPoint3D(2, 2, 2), NodeLocation(octree.root()));
+	EXPECT_EQ(iPoint3D(0, 0, 0), NodeExtent(octree.root()[0][0]));
+	EXPECT_EQ(iPoint3D(1, 1, 1), NodeExtent(octree.root()[0]));
+}
+
 /*
 	expand(octree, voxels);
 	cout.write(&(voxels.begin()[0]), voxels.size());
@@ -16,4 +47,4 @@ TEST(DTreeNode, NodeLocation) {
 	cout << octree.max_depth() << endl;
 	cout << NodeLocation(octree.root()[0]) << " " << NodeExtent(octree.root()[0])  << endl;
 */
-}
+

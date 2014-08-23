3a4f3c279c2b814d6a242ba95b2a0ab0c80c6762
Michał Januszewski <michalj@gmail.com>
2014-08-23 23:21:25 +0200

    Make save as numpy work with both array orders.

--- utils/voxelizer/io.hpp
+++ utils/voxelizer/io.hpp
@@ -1,3 +1,11 @@
+
+// Simple logging for vectors.
+template <typename T>
+std::ostream& operator <<(std::ostream& os, const std::vector<T>& v) {
+    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os));
+    return os;
+}
+
 void SaveAsNumpy(const cvmlcpp::Matrix<char, 3u>& voxels, const std::string& fname) {
 	std::ofstream out(fname + ".npy");
 	out << "\x93NUMPY\x01";
@@ -5,8 +13,13 @@ void SaveAsNumpy(const cvmlcpp::Matrix<char, 3u>& voxels, const std::string& fna
 	char buf[128] = {0};
 	out.write(buf, 1);
 	const std::size_t *ext = voxels.extents();
-	snprintf(buf, 128, "{'descr': 'bool', 'fortran_order': False, 'shape': (%lu, %lu, %lu)}",
-			ext[0], ext[1], ext[2]);
+	if (voxels.colMajor()) {
+		snprintf(buf, 128, "{'descr': 'bool', 'fortran_order': False, 'shape': (%lu, %lu, %lu)}",
+				ext[2], ext[1], ext[0]);
+	} else {
+		snprintf(buf, 128, "{'descr': 'bool', 'fortran_order': False, 'shape': (%lu, %lu, %lu)}",
+				ext[0], ext[1], ext[2]);
+	}
 
 	int i, len = strlen(buf);
 	unsigned short int dlen = (((len + 10) / 16) + 1) * 16;
